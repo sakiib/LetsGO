5dac19e0b93b740036561f80244450a743ad055a
Sakib Md Al Amin <sakibalamin162@gmail.com>
2020-11-18 21:10:54 +0600

    Update main.go

--- Control_Structures/main.go
+++ Control_Structures/main.go
@@ -9,4 +9,12 @@ func main() {
 	for i := 1; i <= n; i++ {
 		fmt.Println(i)
 	}
+	
+	for i := 1; i <= n; i++ {
+		if i % 2 == 0 {
+			fmt.Printf("%v is Even\n", i)
+		} else {
+			fmt.Printf("%v is Odd\n", i)
+		}
+	}
 }
