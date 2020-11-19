940eac6601deba44012b102cd91a7ad1946b6b4d
Sakib Md Al Amin <sakibalamin162@gmail.com>
2020-11-19 12:05:51 +0600

    Update main.go

--- Arrays_Slices_Maps/main.go
+++ Arrays_Slices_Maps/main.go
@@ -76,4 +76,16 @@ func main() {
 	// length & capacity -> they aren't same
 	fmt.Println(len(a))
 	fmt.Println(cap(a))
+	
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	b := a[:] // slice of all elements
+	c := a[3:] // slice from 4th element to end
+	d := a[:6] // slice first 6 elements
+	e := a[3:6] // slice the 4th, 5th, and 6th elements
+	// they all point to same underlying data
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 }
