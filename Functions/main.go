e9659af08553d973de584f54d29e6f9a3a311374
Sakib Md Al Amin <sakibalamin162@gmail.com>
2020-11-21 00:43:01 +0600

    Update main.go

--- Functions/main.go
+++ Functions/main.go
@@ -13,6 +13,37 @@ func isVowel(ch string) bool {
         return false
     }
 }
+
+var luckyNumbers []int
+
+// recursive function to generate numbers with digits 4,7 only (lucky numbers)
+func generateLuckyNumbers(value int) {
+    if value > limit {
+        return
+    }
+    if value > 0 {
+        luckyNumbers = append(luckyNumbers, value)
+    }
+    generateLuckyNumbers(value * 10 + 4)
+    generateLuckyNumbers(value * 10 + 7)
+}
+
+// simple function to check if a number is present in the slices of lucky numbers
+func binarySearch(cnt int) bool {
+    lo := 0
+    hi := len(luckyNumbers) - 1
+    for lo <= hi {
+        mid := (lo + hi) / 2
+        if luckyNumbers[mid] == cnt {
+            return true
+        } else if luckyNumbers[mid] > cnt {
+            hi = mid - 1
+        } else {
+            lo = mid + 1
+        }
+    }
+    return false
+}
  
 func main() {
 	  
