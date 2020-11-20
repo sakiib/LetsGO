f5f6dff049415dd00f044e9e18a3e93d9f5930fd
Sakib Md Al Amin <sakibalamin162@gmail.com>
2020-11-21 00:41:00 +0600

    Create main.go

---
+++ Functions/main.go
@@ -0,0 +1,19 @@
+package main
+ 
+import (
+	"fmt"
+	"strings"
+)
+
+// func function_name (argument) return type
+func isVowel(ch string) bool {
+    if ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u" || ch == "y" {
+        return true
+    } else {
+        return false
+    }
+}
+ 
+func main() {
+	  
+}
