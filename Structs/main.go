286c54f88d3c62b08b328f35a9b18b648195cbbd
Sakib Md Al Amin <sakibalamin162@gmail.com>
2020-11-21 14:30:34 +0600

    Update main.go

--- Structs/main.go
+++ Structs/main.go
@@ -13,4 +13,14 @@ func main() {
 	person1 := Person{Name: "sakib", Id: 1}
 	fmt.Println(person1)
 	fmt.Println(person1.Name, person1.Id)
+	
+	var personList []Person
+	personList = append(personList, Person{Name: "messi", Id: 1})
+    	personList = append(personList, Person{Name: "cr7", Id: 3})
+   	personList = append(personList, Person{Name: "sakib", Id: 4})
+    	personList = append(personList, Person{Name: "alamin", Id: 2})
+
+    	for _, person := range personList {
+		fmt.Println(person.Name, person.Id)
+    	}
 }
