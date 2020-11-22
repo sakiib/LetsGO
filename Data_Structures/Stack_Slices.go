da420b512e09929b7f80b230f72ed1672054cd4d
Sakib Md Al Amin <sakibalamin162@gmail.com>
2020-11-22 19:27:41 +0600

    Create Stack_Slices.go

---
+++ Data_Structures/Stack_Slices.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	stack := make([] int, 0)
+	stack = append(stack, 1)
+	stack = append(stack, 2)
+	stack = append(stack, 3)
+	fmt.Println(stack)
+	for len(stack) > 0 {
+	    n := len(stack) - 1
+        fmt.Println(stack[n])
+        stack = stack[:n] 
+	}
+}
