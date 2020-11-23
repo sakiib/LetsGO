e7932ae76071c07f554c033cd6c92a392ffd858b
Sakib Md Al Amin <sakibalamin162@gmail.com>
2020-11-23 21:56:28 +0600

    Update sort_slices_int.go

--- Sorting/sort_slices_int.go
+++ Sorting/sort_slices_int.go
@@ -9,4 +9,7 @@ func main() {
 	val := []int{3, 2, 1}
 	sort.Ints(val)
 	fmt.Println(val)
+	
+	// to sort an array []a, use it like a slice [l, r) -> r is exclusive, array indices 1 to n
+	sort.Ints(a[1 : n+1])
 }
